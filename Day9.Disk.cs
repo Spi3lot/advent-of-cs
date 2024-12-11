214fdbb4eb019eb46b7f97b974ca15227dcd242c
Spi3lot <spi3lot@gmail.com>
2024-12-12 00:30:40 +0100

    Continue solving day 9; Improve code of day 8

--- Day9.Disk.cs
+++ Day9.Disk.cs
@@ -1,54 +1,68 @@
 ﻿namespace AdventOfCode;
+
 public partial record Day9
 {
 
-    private sealed class Disk(ICollection<(int? id, int length)> files)
+    private sealed record Disk(IList<FileFragment> Fragments)
     {
-
-        public readonly ICollection<(int? id, int length)> Files = files;
 
         public static Disk FromMap(string map)
         {
-            ICollection<(int? id, int length)> files = [];
+            IList<FileFragment> files = [];
 
             for (int i = 0; i < map.Length; i++)
             {
-                int? id = (i % 2 == 0) ? i / 2 : null;
+                int? fileId = i % 2 == 0 ? i / 2 : null;
                 int length = map[i] - '0';
-                files.Add((id, length));
-                Console.WriteLine((id, length));
+                files.Add(new FileFragment(fileId, length));
             }
 
             return new Disk(files);
         }
 
+        public void Defragment()
+        {
+            for (int i = Fragments.Count - 1; i >= 0; i--)
+            {
+                var movingFragment = Fragments[i];
+                // TODO
+            }
+        }
+
         public int CalculateChecksum()
         {
             int checksum = 0;
+            int position = 0;
+            int previousSum = 0;
 
-            for (int i = 0; ; i++)
+            foreach (var fragment in Fragments)
             {
-                var (id, length) = Files.ElementAt(i);
-                //cumulativeSum += GetSum(i..(i + length));
-                checksum += i * ;
+                if (fragment.FileId == null) continue;
+                position += fragment.Length;
+                int sum = CalcSumUpToExcl(position);
+                checksum += sum - previousSum;
+                previousSum = sum;
             }
 
             return checksum;
         }
 
-        private static int GetSum(Range range)
+        private static int CalcSumUpToExcl(int n)
         {
-            return GetSumUpTo(range.End.Value) - GetSumUpTo(range.Start.Value);
-        }
-
-        private static int GetSumUpTo(int n)
-        {
-            return n * (n + 1) / 2;
+            return n * (n - 1) / 2;
         }
 
         public override string ToString()
         {
-            var chars = Files.SelectMany(file => new string(file.id == null ? '.' : $"{file.id}"[0], file.length)).ToArray();
+            char[] chars = Fragments
+                .SelectMany(fragment => new string(
+                    (fragment.FileId == null)
+                        ? '.'
+                        : $"{fragment.FileId}"[0],
+                    fragment.Length
+                    ))
+                .ToArray();
+
             return new string(chars);
         }
 
--- Day9.cs
+++ Day9.cs
@@ -4,7 +4,8 @@ public partial record Day9 : AdventDay<Day9>
     public override void SolvePart1()
     {
         var disk = Disk.FromMap(Input);
-        Console.WriteLine(disk);
+        disk.Defragment();
+        Console.WriteLine(disk.CalculateChecksum());
     }
 
     public override void SolvePart2()
