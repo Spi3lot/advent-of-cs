290bdcd3cae5ceeb40838f0d242733acfc87edf2
Spi3lot <spi3lot@gmail.com>
2025-12-11 23:29:37 +0100

    Solve day 5

---
+++ Year2025/Day5.Range.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode.Year2025;
+
+public partial record Day5
+{
+
+    public class Range
+    {
+
+        public required long Start { get; set; }
+
+        public required long End { get; set; }
+
+        public long Count => End - Start + 1;
+        
+        public static Range FromString(string input) => FromArray(input.Split('-').Select(long.Parse).ToArray());
+
+        public static Range FromArray(long[] array)
+        {
+            return (array.Length == 2)
+                ? new Range { Start = array[0], End = array[1] }
+                : throw new ArgumentException("Must provide an array of size 2");
+        }
+
+        public bool Merge(Range range)
+        {
+            var minStartRange = (Start < range.Start) ? this : range;
+            var maxStartRange = (Start < range.Start) ? range : this;
+            var maxEndRange = (End < range.End) ? range : this;
+
+            if (minStartRange.End + 1 < maxStartRange.Start)
+            {
+                return false;
+            }
+
+            Start = minStartRange.Start;
+            End = maxEndRange.End;
+            return true;
+        }
+
+        public override string ToString() => $"{Start}-{End}";
+
+    }
+
+}
---
+++ Year2025/Day5.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.Year2025;
+
+public partial record Day5() : AdventDay<Day5>(2025)
+{
+
+    public override void SolvePart1()
+    {
+        string[][] ids = Input.Trim()
+            .Split("\n\n")
+            .Select(ids => ids.Split('\n'))
+            .ToArray();
+
+        var ranges = ids[0]
+            .Select(Range.FromString)
+            .ToArray();
+
+        int count = ids[1]
+            .Select(long.Parse)
+            .Count(id => ranges.Any(range => range.Start <= id && id <= range.End));
+
+        Console.WriteLine(count);
+    }
+
+
+    public override void SolvePart2()
+    {
+        string[][] ids = Input.Trim()
+            .Split("\n\n")
+            .Select(ids => ids.Split('\n'))
+            .ToArray();
+
+        var ranges = ids[0]
+            .Select(Range.FromString)
+            .OrderBy(range => range.Start)
+            .ToList();
+
+        int i = 0;
+        
+        while (i < ranges.Count - 1)
+        {
+            if (ranges[i].Merge(ranges[i + 1]))
+            {
+                ranges.RemoveAt(i + 1);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        Console.WriteLine(ranges.Sum(range => range.Count));
+    }
+
+}
