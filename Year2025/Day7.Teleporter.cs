c739c8960e42ca74b03fd1ad39ef49f0622df4db
Spi3lot <spi3lot@gmail.com>
2025-12-15 00:17:05 +0100

    Begin solving day 7 part 1

---
+++ Year2025/Day7.Teleporter.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Year2025;
+
+public partial record Day7
+{
+
+    public class Teleporter
+    {
+
+        private readonly List<Beam> _beams = [];
+
+        public int SplitCount { get; private set; }
+
+        public class Beam(Teleporter teleporter)
+        {
+
+            public int X { get; private set; }
+
+            public int Y { get; private set; }
+
+            public void Split()
+            {
+                teleporter.SplitCount++;
+                teleporter._beams.Add(new Beam(teleporter) { X = X, Y = Y });
+                int duplicateCount = teleporter._beams.Count - teleporter._beams.DistinctBy(beam => (beam.X, beam.Y)).Count();
+
+                if (duplicateCount > 0)
+                {
+                    Console.WriteLine(duplicateCount);
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ Year2025/Day7.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Year2025;
+
+public partial record Day7() : AdventDay<Day7>(2025)
+{
+
+    public override void SolvePart1()
+    {
+    }
+
+
+    public override void SolvePart2()
+    {
+    }
+
+}
