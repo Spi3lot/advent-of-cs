b13ac1f46bb1ec9ae4e03bad7eef33a2d3e8b25a
Spi3lot <spi3lot@gmail.com>
2025-12-22 00:28:43 +0100

    Make tremendous progress in solving part 2 of day 9

---
+++ Year2025/Day9.SectionCollection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace AdventOfCode.Year2025;
+
+public class SectionCollection<T> : IEnumerable<(T Item, int Size)>
+{
+
+    private readonly List<T> _sectionItems = [];
+
+    private readonly List<int> _sectionEndIndices = [];
+    
+    public int SectionCount => _sectionItems.Count;
+
+    public void AddSection(T item, int size)
+    {
+        if (size <= 0)
+        {
+            throw new InvalidOperationException("Size must be greater than zero.");
+        }
+
+        int lastIndex = (_sectionItems.Count == 0) ? -1 : _sectionEndIndices[^1];
+        _sectionEndIndices.Add(lastIndex + size);
+        _sectionItems.Add(item);
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            int sectionIndex = _sectionEndIndices.BinarySearch(index);
+            return _sectionItems[(sectionIndex < 0) ? ~sectionIndex : sectionIndex];
+        }
+    }
+
+    public IEnumerator<(T Item, int Size)> GetEnumerator()
+    {
+        int accumulatedLength = -1;
+        
+        for (int i = 0; i < SectionCount; i++)
+        {
+            yield return (_sectionItems[i], _sectionEndIndices[i] - accumulatedLength);
+            accumulatedLength += _sectionEndIndices[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+}
--- Year2025/Day9.cs
+++ Year2025/Day9.cs
@@ -1,4 +1,6 @@
-﻿using Tile = (int X, int Y);
+﻿using System.Collections;
+
+using Tile = (int X, int Y);
 using Rectangle = ((int X, int Y) FirstCorner, (int X, int Y) SecondCorner);
 
 namespace AdventOfCode.Year2025;
@@ -8,9 +10,14 @@ using RectangleArea = (Rectangle Rectangle, long Area);
 public record Day9() : AdventDay<Day9>(2025)
 {
 
+    private static readonly IComparer<RectangleArea> RectangleAreaComparer =
+        Comparer<RectangleArea>.Create((a, b) => a.Area.CompareTo(b.Area));
+
     private readonly List<RectangleArea> _rectangleAreas = [];
 
-    private bool[,] _isInsideShape = new bool[0, 0];
+    private BitArray[]? _borders;
+
+    private SectionCollection<bool>[]? _insideShape;
 
     public override void Setup()
     {
@@ -29,14 +36,75 @@ public record Day9() : AdventDay<Day9>(2025)
             {
                 var rectangle = (tiles[i], tiles[j]);
                 var rectangleArea = (rectangle, CalculateArea(rectangle));
-                int index = ~_rectangleAreas.BinarySearch(rectangleArea);
-                _rectangleAreas.Insert(index, rectangleArea);
+                int index = _rectangleAreas.BinarySearch(rectangleArea, RectangleAreaComparer);
+                _rectangleAreas.Insert((index < 0) ? ~index : index, rectangleArea);
             }
         }
-        
-        // TODO: fill _isInsideShape
 
         _rectangleAreas.Reverse();
+        int width = 3 + tiles.Max(tile => tile.X); // TODO: change 3 back to 1
+        int height = 3 + tiles.Max(tile => tile.Y); // TODO: change 3 back to 1
+        _borders = new BitArray[height];
+        _insideShape = new SectionCollection<bool>[height];
+
+        for (int i = 0; i < height; i++)
+        {
+            _borders[i] = new BitArray(width);
+            _insideShape[i] = new SectionCollection<bool>();
+        }
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            ConnectTiles(tiles[i], tiles[(i + 1) % tiles.Length]);
+        }
+
+        using var border = File.OpenWrite("border.txt");
+
+        foreach (var bits in _borders)
+        {
+            foreach (bool bit in bits)
+            {
+                border.WriteByte((byte) ((bit) ? 'X' : '.'));
+            }
+        
+            border.WriteByte((byte) '\n');
+        }
+        
+        // foreach (var sections in _insideShape)
+        // {
+        //     foreach (var section in sections)
+        //     {
+        //         var buffer = Enumerable.Repeat((byte) ((section.Item) ? 'X' : '.'), section.Size);
+        //         border.Write([..buffer]);
+        //     }
+        //
+        //     border.WriteByte((byte) '\n');
+        // }
+
+        ConvertBorderToShape();
+
+        using var shape = File.OpenWrite("shape.txt");
+
+        foreach (var bits in _borders)
+        {
+            foreach (bool bit in bits)
+            {
+                shape.WriteByte((byte) ((bit) ? 'X' : '.'));
+            }
+        
+            shape.WriteByte((byte) '\n');
+        }
+
+        // foreach (var sections in _insideShape)
+        // {
+        //     foreach (var section in sections)
+        //     {
+        //         var buffer = Enumerable.Repeat((byte) ((section.Item) ? 'X' : '.'), section.Size);
+        //         border.Write([..buffer]);
+        //     }
+        //
+        //     shape.WriteByte((byte) '\n');
+        // }
     }
 
     public override void SolvePart1()
@@ -59,7 +127,81 @@ public record Day9() : AdventDay<Day9>(2025)
 
     public bool IsInsideShape(Rectangle rectangle)
     {
-        return _isInsideShape[rectangle.FirstCorner.X, rectangle.FirstCorner.Y];
+        var coordinates = MinMax(rectangle.FirstCorner, rectangle.SecondCorner);
+
+        for (int y = coordinates.Min.Y + 1; y < coordinates.Max.Y; y++)
+        {
+            for (int x = coordinates.Min.X + 1; x < coordinates.Max.X; x++)
+            {
+                if (!_insideShape![y][x])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void ConnectTiles(Tile firstTile, Tile secondTile)
+    {
+        var coordinates = MinMax(firstTile, secondTile);
+
+        if (firstTile.X == secondTile.X)
+        {
+            for (int y = coordinates.Min.Y; y <= coordinates.Max.Y; y++)
+            {
+                _borders![y][firstTile.X] = true;
+            }
+        }
+        else if (firstTile.Y == secondTile.Y)
+        {
+            for (int x = coordinates.Min.X; x <= coordinates.Max.X; x++)
+            {
+                _borders![firstTile.Y][x] = true;
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("The attempted connection does not follow the path of a straight line");
+        }
+    }
+
+    private void ConvertBorderToShape()
+    {
+        // TODO: distingush between 90° left and right turns
+        foreach (var bits in _borders!)
+        {
+            bool passedBorder = false;
+            bool inside = false;
+
+            for (int i = 0; i < bits.Count; i++)
+            {
+                if (bits[i])
+                {
+                    if (!passedBorder)
+                    {
+                        inside = !inside;
+                    }
+
+                    passedBorder = true;
+                }
+                else
+                {
+                    bits[i] = inside;
+                    passedBorder = false;
+                }
+            }
+        }
+    }
+
+    private static (Tile Min, Tile Max) MinMax(Tile firstTile, Tile secondTile)
+    {
+        int minX = Math.Min(firstTile.X, secondTile.X);
+        int minY = Math.Min(firstTile.Y, secondTile.Y);
+        int maxX = Math.Max(firstTile.X, secondTile.X);
+        int maxY = Math.Max(firstTile.Y, secondTile.Y);
+        return ((minX, minY), (maxX, maxY));
     }
 
 }
