27ba93b4e29c6539978902605e87b541ab8846e2
Zottel Emilio <e.zottel@pke.at>
2025-12-23 15:11:59 +0100

    - Implement solution for day 10 part 2 - It works for the example and it probably would for the real thing as well but it's just way too slow

--- Year2025/Day10.Machine.cs
+++ Year2025/Day10.Machine.cs
@@ -8,17 +8,11 @@ public partial record Day10
     public class Machine
     {
 
-        public int LightBitmask { get; }
-
-        public int[] ButtonBitmasks { get; }
-
-        public int[] Joltages { get; }
-
         public Machine(string input)
         {
             string[] parts = input.Split(' ');
             LightBitmask = Lights.GetBitmask(parts[0]);
-            ButtonBitmasks = parts[1..^1].Select(Button.GetBitmask).ToArray();
+            Buttons = parts[1..^1].Select(Button.Parse).ToArray();
 
             Joltages = parts[^1].Trim("{}")
                 .ToString()
@@ -27,20 +21,26 @@ public partial record Day10
                 .ToArray();
         }
 
+        public int LightBitmask { get; }
 
-        public int DetermineMinimumButtonPresses(bool joltage)
+        public Button[] Buttons { get; }
+
+        public int[] Joltages { get; }
+
+
+        public int DetermineMinimumButtonPressCount(bool joltage)
         {
-            return (joltage)
-                ? GetValidJoltageButtonPressCounts().Min()
+            return joltage
+                ? GetValidJoltageButtonPressCounts().Min(Enumerable.Sum)
                 : GetValidLightButtonPressMasks().Min(BitOperations.PopCount);
         }
 
         public IEnumerable<uint> GetValidLightButtonPressMasks()
         {
-            for (int mask = 0; mask != (1 << ButtonBitmasks.Length); mask++)
+            for (int mask = 0; mask != 1 << Buttons.Length; mask++)
             {
-                int lights = ButtonBitmasks.Where((_, index) => (mask & (1 << index)) != 0)
-                    .Aggregate(0, (result, buttonBitmask) => result ^ buttonBitmask);
+                int lights = Buttons.Where((_, index) => (mask & (1 << index)) != 0)
+                    .Aggregate(0, (result, button) => result ^ button.Bitmask);
 
                 if (lights == LightBitmask)
                 {
@@ -49,16 +49,67 @@ public partial record Day10
             }
         }
 
-        public IEnumerable<int> GetValidJoltageButtonPressCounts()
+        public IEnumerable<int[]> GetValidJoltageButtonPressCounts()
         {
-            yield break;
+            return GetValidJoltageButtonPressCounts(
+                0,
+                new int[Buttons.Length],
+                new int[Joltages.Length]
+            );
+        }
+
+        private IEnumerable<int[]> GetValidJoltageButtonPressCounts(
+            int buttonIndex,
+            int[] pressCounts,
+            int[] joltages
+        )
+        {
+            if (buttonIndex >= Buttons.Length)
+            {
+                yield break;
+            }
+
+            do
+            {
+                var deeperPressCounts = GetValidJoltageButtonPressCounts(
+                    buttonIndex + 1,
+                    [..pressCounts],
+                    [..joltages]
+                );
+
+                foreach (int[] counts in deeperPressCounts)
+                {
+                    yield return counts;
+                }
+            } while (TryPressJoltageButton(buttonIndex, pressCounts, joltages));
+
+            if (joltages.SequenceEqual(Joltages))
+            {
+                yield return pressCounts;
+            }
+        }
+
+        private bool TryPressJoltageButton(int buttonIndex, int[] pressCounts, int[] joltages)
+        {
+            if (!Buttons[buttonIndex].Wiring.All(pin => joltages[pin] + 1 <= Joltages[pin]))
+            {
+                return false;
+            }
+
+            foreach (int pin in Buttons[buttonIndex].Wiring)
+            {
+                joltages[pin]++;
+            }
+
+            pressCounts[buttonIndex]++;
+            return true;
         }
 
         public IEnumerable<int> GetMaxJoltageButtonPressCounts()
         {
-            yield break;
+            return Buttons.Select(button => button.Wiring.Min(pin => Joltages[pin]));
         }
-        
+
     }
 
 }
