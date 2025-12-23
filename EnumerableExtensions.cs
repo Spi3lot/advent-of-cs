d1d201a454d1a8ec8921e3c33c609731a4b123a4
Zottel Emilio <e.zottel@pke.at>
2025-12-23 15:06:25 +0100

    Add LazyForEach methods for both IEnumerable and ParallelQuery to simplify enumeration after iteration.

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode;
+
+public static class EnumerableExtensions
+{
+
+    extension<T>(IEnumerable<T> enumerable)
+    {
+
+        public IEnumerable<T> LazyForEach(Action<T> action)
+        {
+            return enumerable.Select(item =>
+            {
+                action(item);
+                return item;
+            });
+        }
+
+        public IEnumerable<T> LazyForEach(Action<T, int> action)
+        {
+            return enumerable.Select((item, index) =>
+            {
+                action(item, index);
+                return item;
+            });
+        }
+
+    }
+
+    extension<T>(ParallelQuery<T> parallelQuery)
+    {
+
+        public ParallelQuery<T> LazyForAll(Action<T> action)
+        {
+            return parallelQuery.Select(item =>
+            {
+                action(item);
+                return item;
+            });
+        }
+
+        public ParallelQuery<T> LazyForAll(Action<T, int> action)
+        {
+            return parallelQuery.Select((item, index) =>
+            {
+                action(item, index);
+                return item;
+            });
+        }
+
+    }
+
+}
